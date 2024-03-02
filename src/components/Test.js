18a91941b39cb51f8b48ee6f7acd039716342db9
collinshen123 <collinshen123@gmail.com>
2024-03-01 16:57:27 -0800

    added submit button on test

--- src/components/Test.js
+++ src/components/Test.js
@@ -68,6 +68,8 @@ function Body() {
                 {DATA.map((question) => {
                     return <CreateQuestion question={question} />
                 })}
+                <input type="submit" value="Submit" />
+
             </form>
             
           </body>
@@ -78,9 +80,9 @@ function CreateQuestion({question}) {
     return (
         <div className="question">
             <h2>{question.Question}</h2>
-            <input type="radio" name={question.name} value="A" id="${question.name}A" /><label for="${question.name}A"> {question.A}</label><br />
-            <input type="radio" name={question.name} value="B" id="${question.name}B" /><label for="${question.name}B"> {question.B}</label><br />
-            <input type="radio" name={question.name} value="C" id="${question.name}C" /><label for="${question.name}C"> {question.C}</label><br />
+            <input type="radio" name={question.name} value="A" id="${question.name}A" /><label for="${question.name}A">{question.A}</label><br />
+            <input type="radio" name={question.name} value="B" id="${question.name}B" /><label for="${question.name}B">{question.B}</label><br />
+            <input type="radio" name={question.name} value="C" id="${question.name}C" /><label for="${question.name}C">{question.C}</label><br />
         </div>
     )
 }
